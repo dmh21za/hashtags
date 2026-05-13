c8b54bdde115e3095e8ea4a1bbf7ac0da324c64a
Marcus <dmh21sta@gmail.com>
2026-05-13 16:04:58 +0100

    HasHashtagAnalyser supports instagram datasets now in addition to Facebook ones

--- HasHashtagAnalyser.py
+++ HasHashtagAnalyser.py
@@ -2,8 +2,22 @@ from Analyser import Analyser
 import pandas as pd
 import numpy as np
 import re
+from enum import Enum
 
-ENGAGEMENT_COLUMN_NAMES = ['Views', 'Reach', 'Shares', 'Reactions', 'Comments']
+class Platform(Enum):
+    FACEBOOK = 'facebook'
+    INSTAGRAM = 'instagram'
+
+
+ENGAGEMENT_COLUMN_NAMES = {
+    Platform.INSTAGRAM: ['Views', 'Reach', 'Likes', 'Shares', 'Comments', 'Follows'],
+    Platform.FACEBOOK: ['Views', 'Reach', 'Shares', 'Reactions', 'Comments']
+}
+
+PIVOT_COLUMN_INCLUDED_VALUES = {
+    Platform.FACEBOOK: ['Photos', 'Videos'],
+    Platform.INSTAGRAM: ['IG image', 'IG reel']
+}
 
 class HasHashtagAnalyser(Analyser):
     
@@ -11,11 +25,16 @@ class HasHashtagAnalyser(Analyser):
     @staticmethod
     def append_has_hashtag(df : pd.DataFrame) -> pd.DataFrame:
         df = df.copy()
-        df['Text'] = np.where(
-            df['Post type'] == 'Videos',
-            df['Description'],
-            df['Title']
-        )
+        if 'Description' in df.columns and 'Title' in df.columns:
+            # handles facebook data
+            df['Text'] = np.where(
+                df['Post type'] == 'Videos',
+                df['Description'],
+                df['Title']
+            )
+        else:
+            # handles instagram data
+            df['Text'] = df['Description']
 
         # Set all missing text to ""
         df['Text'] = df['Text'].fillna("")
@@ -29,16 +48,14 @@ class HasHashtagAnalyser(Analyser):
         return df
 
 
-    def __init__(self, csv_location : str):
+    def __init__(self, csv_location : str, platform : Platform, target_columns : list[str] = None):
         df = pd.read_csv(csv_location)
-
         df = HasHashtagAnalyser.append_has_hashtag(df)
-
         super().__init__(
             df = df,
             pivot_column = 'Post type',
             predictor_column = 'HasHashtag',
-            target_columns = ENGAGEMENT_COLUMN_NAMES,
-            pivot_column_included_values=['Photos', 'Videos']
+            target_columns = target_columns or ENGAGEMENT_COLUMN_NAMES[platform],
+            pivot_column_included_values= PIVOT_COLUMN_INCLUDED_VALUES[platform]
         )
         
